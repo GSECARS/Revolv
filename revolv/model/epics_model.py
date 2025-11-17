17e59662d430c3550a679c8e65e9571db87d1993
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2025-11-17 14:07:54 -0600

    adds photodiode pv

--- revolv/model/epics_model.py
+++ revolv/model/epics_model.py
@@ -58,6 +58,7 @@ class EpicsConfig(Enum):
     detector_acquire = "13IDD:userStringSeq11.PROC"
     detector_exposure = "13EIG2_9M:cam1:AcquireTime"
     detector_armed = "13EIG2_9M:cam1:Acquire"
+    photodiode = "13IDD:Photodiode"
     stop_pvs = ["13IDD:Auto1:allstop.VAL", "13IDD_Linux:allstop.VAL", "13IDD_DAC_XPS16:allstop.VAL"]
 
 
