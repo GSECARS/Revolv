2177cf23978a90b1bff539578a6b818d1a7c4490
Christofanis Skordas <skordasc@uchicago.edu>
2026-09-18 00:44:24 -0500

    switches to WxApplication and adds a test app menu

--- revolv/__init__.py
+++ revolv/__init__.py
@@ -2,24 +2,46 @@
 
 from argparse import ArgumentParser
 from importlib.metadata import version
+from pathlib import Path
+
+from wxutils import AppConfig, WxApplication, add_shortcut_arguments, handle_shortcut_arguments
+
+from revolv.controller.main_controller import MainController
+from revolv.view import MainView
 
 __version__ = version("revolv")
 __all__ = ["__version__", "main"]
 
+APP_CONFIG = AppConfig(
+    name="Revolv",
+    assets=str(Path(__file__).resolve().parent / "assets"),
+    description="X-ray scanning and data collection application",
+    application_id="gsecars.revolv",
+)
+
+
+def make_parser():
+    """Build the revolv command line parser."""
+    parser = ArgumentParser("revolv")
+    add_shortcut_arguments(parser)
+    return parser
+
+
+def start_ui():
+    """Construct and start the GUI."""
+    app = WxApplication(APP_CONFIG, False)
+    view = MainView(version=__version__)
+    _controller = MainController(view=view)
+
+    app.run(view)
+
 
 def main() -> None:
     """Main revolv entry point."""
-    parser = ArgumentParser()
-    parser.add_argument("-m", "--make-icon", action="store_true", help="creates a desktop/application shortcut")
-    parser.add_argument("-p", "--public", dest="public_folder", nargs="?", const="GSE Apps", metavar="FOLDER", help="creates a public shortcut")
+    parser = make_parser()
     args = parser.parse_args()
 
-    if args.make_icon:
-        from revolv.app import create_shortcut
-
-        create_shortcut(public=args.public_folder is not None, folder=args.public_folder)
+    if handle_shortcut_arguments(parser, args, APP_CONFIG):
         return
-
-    from revolv.app import start_ui
 
     start_ui()
--- revolv/view/main_view.py
+++ revolv/view/main_view.py
@@ -1,8 +1,5 @@
 # SPDX-License-Identifier: MIT
 
-import sys
-from importlib.resources import as_file, files
-
 import wx
 
 
@@ -12,18 +9,19 @@ class MainView(wx.Frame):
     def __init__(self, version: str) -> None:
         super(MainView, self).__init__(parent=None, id=wx.ID_ANY, title=f"Revolv {version}")
 
-        self._set_icon()
+        self._create_menu()
 
-    def _set_icon(self) -> None:
-        """Sets the application icon."""
-        if sys.platform == "win32":
-            filename = "revolv.ico"
-            bitmap_type = wx.BITMAP_TYPE_ICO
-        else:
-            filename = "revolv.png"
-            bitmap_type = wx.BITMAP_TYPE_PNG
-
-        resource = files("revolv").joinpath("assets", filename)
-
-        with as_file(resource) as path:
-            self.SetIcon(wx.Icon(str(path), bitmap_type))
+    def _create_menu(self) -> None:
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        file_menu.Append(
+            wx.ID_EXIT,
+            "Quit\tCtrl+Q",
+        )
+        menu_bar.Append(file_menu, "&File")
+        self.SetMenuBar(menu_bar)
+        self.Bind(
+            wx.EVT_MENU,
+            lambda event: self.Close(),
+            id=wx.ID_EXIT,
+        )
